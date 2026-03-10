ad36cd07bee85ae1c217c93b8853abf7cd4cd331
Dhnnz12 <dhonanhibrizi123@gmail.com>
2026-03-10 21:57:36 +0700

    Mmebuat Service Ktp

---
+++ src/main/java/com/example/demo/service/KtpService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Ktp;
+
+import java.util.List;
+
+public interface KtpService {
+    Ktp create(Ktp ktp);
+    List<Ktp> getAll();
+    Ktp getById(Integer id);
+    Ktp update(Integer id, Ktp ktp);
+    void delete(Integer id);
+}
